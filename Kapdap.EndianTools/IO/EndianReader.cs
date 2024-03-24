64ba4cfbb85722edd871c0f9d37a52d42e865be2
Kapdap <kapdap@pm.me>
2024-03-24 21:50:09 +1300

    More char byte order fixes

--- Kapdap.EndianTools/IO/EndianReader.cs
+++ Kapdap.EndianTools/IO/EndianReader.cs
@@ -231,11 +231,10 @@ namespace Kapdap.EndianTools.IO
             int i = 0;
             while (Position < Length)
             {
-                if (charSize == 2)
-                    charBytes[1] = (byte)BaseStream.ReadByte();
+                charBytes[0] = (byte)BaseStream.ReadByte();
 
-                if (Position < Length)
-                    charBytes[0] = (byte)BaseStream.ReadByte();
+                if (charSize == 2 && Position < Length)
+                    charBytes[1] = (byte)BaseStream.ReadByte();
 
                 chars[i++] = encoding.GetChars(charBytes, 0, charSize)[0];
 
